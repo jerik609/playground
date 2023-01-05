3d71b1065e17830516f778652b4a65ce083ad1bb
jerik <michal.jancok@gmail.com>
2023-01-06 00:34:26 +0100

    weird pipeline overriding

--- src/CompletableFutures/PipelineWeird.java
+++ src/CompletableFutures/PipelineWeird.java
@@ -6,16 +6,20 @@ import static java.lang.Thread.sleep;
 
 public class PipelineWeird {
     public static void main(String[] args) throws InterruptedException {
-        CompletableFuture<Integer> noFuture = new CompletableFuture<Integer>()
+        CompletableFuture<Integer> noFuture = new CompletableFuture<Integer>();
+        noFuture
                 .thenApply(item -> item * item)
                 .thenApply(item -> item + 10);
+                //.thenAccept(System.out::println);
 
         System.out.println("prepared the pipeline");
 
-        noFuture.thenAccept(System.out::println);
+        noFuture.thenApply(x -> x + 1234).thenAccept(System.out::println);
         // ^^^^^ THIS WOULD NOT WORK, NOTICE WE SET THIS TO THE ORIGINAL FUTURE
         // NOT THE RESULT OF THE PIPELINE WE BUILT EARLIER!
         // BE CAREFUL WHEN CHAINING LIKE THIS!
+
+        // WE ARE OVERRIDING THE PIPELINE! somehow ....
 
         System.out.println("prepared the output");
 
