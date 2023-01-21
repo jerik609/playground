d5588fbeaa11d9f487f21a88600abcfcfd0e2629
Michal <117671671+jerik609@users.noreply.github.com>
2023-01-21 17:02:25 +0100

    user vs daemon threads

---
+++ src/Executing/Main.java
@@ -0,0 +1,29 @@
+package Executing;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //ExecutorService executor = Executors.newSingleThreadExecutor();
+        ExecutorService executor = new ForkJoinPool();
+
+        Runnable task = () -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+                System.out.println("Hello!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+        executor.submit(task);
+        executor.submit(task);
+
+        System.out.println("Main thread done");
+
+    }
+}
